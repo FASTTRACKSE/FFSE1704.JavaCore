9df06e40639a7c3adc98e024204f1b82456ce368
bùi vũ <FFSE1702044@st.fasttrack.edu.vn>
2018-05-30 14:30:51 +0700

    readme.md

--- FFSE1702044_BuiVu/src/CtNhapDuLieuChoMang/CtNhapDuLieuChoMang.java
+++ FFSE1702044_BuiVu/src/CtNhapDuLieuChoMang/CtNhapDuLieuChoMang.java
@@ -8,38 +8,41 @@ import java.util.Scanner;
 public class CtNhapDuLieuChoMang {
 
 	public static void main(String[] args) {
-		//Nhập Số lượng
-		int N;
+		//Khai Báo
+		int N;	
+		
+		Scanner myInput = new Scanner (System.in);
 		System.out.print("Nhập Số lượng mảng : ");
-		int a[]= new int[50];
-		Scanner nhap = new Scanner (System.in);
-		N = nhap.nextInt();
+		N = myInput.nextInt();
+		int[] myArray = new int[N];
+		
 		//Kiểm tra Số lượng muốn tạo, và cho phép người dùng nhập giá trị trong mảng.
 		for(int i=0;i<N;i++) {
-			a[i]= nhap.nextInt();
+			System.out.print("Nhập Phần tử thứ "+ (i+1)+ " của mảng: ");
+			myArray[i]= myInput.nextInt();
 		}
+		//
 		for(int i=0;i<N;i++) {
-			System.out.println("Số Thứ "+i+" trong mảng là : " + a[i]);
+			System.out.println("Số Thứ "+(i+1)+" trong mảng là : " + myArray[i]);
 		}
 		//Bài 2+3 tìm giá trị và vị trí lớn bé cho mảng
-		int max=a[0];
-		int min=a[0];
-		int i;
-		int maxi=0;
-		int mini=0;
-		for (i=0;i<N;i++)
-        {
-            if(max<a[i]) {
-                max=a[i];
-                maxi=i;
-            }
-            if(min>a[i]) {
-             min=a[i];
-             mini=i;
-            }
-        }
-        System.out.println("Giá Trị Lớn nhất là "+ max +" Vị Trí của số lớn nhất là : "+ maxi);
-        System.out.println("Giá Trị Nhỏ nhất là "+ min + " Vị Trí của số nhỏ nhất là : "+ mini);
+		int gtMin,gtMax,vtMin,vtMax;
+		gtMin=myArray[0];
+		gtMax=myArray[0];
+		vtMin=0;
+		vtMax=0;
+		for(int i=1;i<N;i++) {
+			if(gtMin>myArray[i]) {
+				gtMin=myArray[i];
+				vtMin= (i+1);
+			}
+			if(gtMax<myArray[i]) {
+				gtMax=myArray[i];
+				vtMax= (i+1);
+			}
+		}
+        System.out.println("Giá Trị Lớn nhất là "+ gtMax +" Vị Trí của số lớn nhất là : "+ vtMax);
+        System.out.println("Giá Trị Nhỏ nhất là "+ gtMin + " Vị Trí của số nhỏ nhất là : "+ vtMin);
         
 	}
 
