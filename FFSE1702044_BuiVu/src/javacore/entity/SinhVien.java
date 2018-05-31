15f5cda6f38e0fdf56c62e801a8bc6be1f670261
bùi vũ <FFSE1702044@st.fasttrack.edu.vn>
2018-05-31 21:10:37 +0700

    readme.md

--- FFSE1702044_BuiVu/src/javacore/entity/SinhVien.java
+++ FFSE1702044_BuiVu/src/javacore/entity/SinhVien.java
@@ -3,10 +3,12 @@ package javacore.entity;
 import java.util.Scanner;
 
 public class SinhVien {
-	String fullName;
-	int maSinhVien,LP1,LP2,LP3;
-	float diemTrungBinh;
-	public void Input() {
+	private String tenSinhVien;
+	private int namSinh;
+	private int diemLP1;
+	private int diemLP2;
+	private int diemLP3;
+	/*public void Input() {
 		Scanner myInput = new Scanner(System.in);
 		System.out.print("Nhập Tên Sinh Viên : ");
 		fullName = myInput.nextLine();
@@ -20,8 +22,8 @@ public class SinhVien {
 		LP3 =myInput.nextInt();
 		}
 	
-	public void DiemTrungBinh() {
-		diemTrungBinh=(LP1+LP2+LP3)/3;
+	public float DiemTrungBinh() {
+		return (LP1+LP2+LP3)/3;
 	}
 	public void Output() {
 		System.out.println("+--------------------------------+");
@@ -30,5 +32,57 @@ public class SinhVien {
 		System.out.println("|Điểm LP1 "+LP1+" Điểm LP2 "+LP2+" Điểm LP3 "+LP3+"|");
 		System.out.println("|Điểm Trung Bình "+diemTrungBinh+"             |");
 		System.out.println("+--------------------------------+");
+	}*/
+	public SinhVien(String tenSinhVien,int namSinh,int diemLP1,int diemLP2,int diemLP3) {
+		this.tenSinhVien=tenSinhVien;
+		this.namSinh=namSinh;
+		this.diemLP1=diemLP1;
+		this.diemLP2=diemLP2;
+		this.diemLP3=diemLP3;
 	}
+	public String getSinhVien(){
+		return tenSinhVien;
+	}
+	public void setTenSinhVien(String tenSinhVien) {
+		this.tenSinhVien = tenSinhVien;
+	}
+	public void setNamSinh(int namSinh) {
+		this.namSinh = namSinh;
+	}
+	
+	public int getDiemLP1() {
+		return diemLP1;
+	}
+
+	public void setDiemLP1(int diemLP1) {
+		this.diemLP1 = diemLP1;
+	}
+
+	public int getDiemLP2() {
+		return diemLP2;
+	}
+
+	public void setDiemLP2(int diemLP2) {
+		this.diemLP2 = diemLP2;
+	}
+
+	public int getDiemLP3() {
+		return diemLP3;
+	}
+
+	public void setDiemLP3(int diemLP3) {
+		this.diemLP3 = diemLP3;
+	}
+	
+	public double tinhDiemTrungBinhMon() {
+		return Math.round((diemLP1 + diemLP2 + diemLP3)/ 3);
+	}
+	/*public String xepLoai() {
+		return xepLoai();
+		
+	}*/
+	public String toString() {
+		return "Sinh Viên: " + this.tenSinhVien + "  Năm Sinh " + this.namSinh;
+	}
+	
 }
--- FFSE1702044_BuiVu/src/javacore/main/QuanLySinhVien.java
+++ FFSE1702044_BuiVu/src/javacore/main/QuanLySinhVien.java
@@ -2,22 +2,24 @@ package javacore.main;
 
 import java.util.Scanner;
 
-import javacore.entity.SinhVien;
+import javacore.entity.*;
 
 public class QuanLySinhVien {
 
 	public static void main(String[] args) {
-		int N;	
-		
 		Scanner myInput = new Scanner (System.in);
-		System.out.print("Nhập Số Sinh Viên : \n");
+
+		/*int N;	
+		
+				System.out.print("Nhập Số Sinh Viên : \n");
 		N = myInput.nextInt();
 		SinhVien[] dsSinhVien = new SinhVien[N];
 		for(int i=0;i<N;i++) {
-			
-			dsSinhVien[i]= new SinhVien();
-			dsSinhVien[i].Input();
-			dsSinhVien[i].DiemTrungBinh();
+			System.out.print("Nhap ten sinh vien: ");
+			String ten = myInput.nextLine();
+			System.out.print("Nhap nam sinh sinh vien: ");
+			int namSinh = myInput.nextInt();
+			dsSinhVien = SinhVien(ten, namSinh);
 		}
 		// dừng chp phép nhập
 		myInput.close();
@@ -31,7 +33,68 @@ public class QuanLySinhVien {
 		
 		//sv1 = new SinhVien();
 		//sv1.Input();
-		//sv1.Output();
+		//sv1.Output();*/
+		int N;	
+		
+		System.out.print("Nhập Số Sinh Viên : \n");
+		N = myInput.nextInt();
+		SinhVien[] sv = new SinhVien[N];
+		for(int i=0;i<N;i++) {
+		System.out.print("Nhap ten sinh vien: ");
+		myInput.nextLine();
+		String tenSinhVien = myInput.nextLine();
+		System.out.print("Nhap nam sinh sinh vien: ");
+		int namSinh = myInput.nextInt();
+		System.out.print("Nhap Điểm LP1 sinh vien: ");
+		int diemLP1 = myInput.nextInt();
+		System.out.print("Nhap Điểm LP2 sinh vien: ");
+		int diemLP2 = myInput.nextInt();
+		System.out.print("Nhap Điểm LP3 sinh vien: ");
+		int diemLP3 = myInput.nextInt();
+		sv[i]= new SinhVien(tenSinhVien, namSinh, diemLP1, diemLP2, diemLP3);
+		sv[i].tinhDiemTrungBinhMon();
+		/*if(sv[i].tinhDiemTrungBinhMon()<=5) {
+			
+			System.out.println("Yếu");
+			
+		}else if(sv[i].tinhDiemTrungBinhMon()<=7) {
+			
+			System.out.println("Trung Bình");
+			
+		}else if(sv[i].tinhDiemTrungBinhMon()<=8.5) {
+			
+			System.out.println("Khá");
+			
+		}else  {
+			
+			System.out.println("Giỏi");
+			
+		}
+		*/
+		
+		
+		}
+		for(int i=0;i<N;i++) {
+			System.out.println(sv[i].toString()+ " -- Điểm Trung Bình "+sv[i].tinhDiemTrungBinhMon());
+			
+	}
+		for(int i=0; i< N-1;i++) {
+        	for(int j=i+1;j<=N-1;j++) {
+        		if(sv[j].tinhDiemTrungBinhMon() < sv[i].tinhDiemTrungBinhMon()) {
+        			SinhVien temp = sv[i];
+        			sv[i]=sv[j];
+        			sv[j]=temp;
+        		}
+        	}
+        }
+        
+		// In thông tin sinh viên 1
+		
+		System.out.println("Điểm Trung Bình sắp xếp nhỏ đến lớn là");
+        for(int i=0;i<N;i++) {
+        	System.out.print( "Sinh Viên"+(i+1) +" : "+sv[i].getSinhVien()  +sv[i].tinhDiemTrungBinhMon()+ " Điểm   ---\t");
+        	
+        }
 	}
 
 }
