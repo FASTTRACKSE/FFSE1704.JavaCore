d6e0347dc696519c87185120f5939bece2cb1926
Nguyễn Quốc Hưng <ffse1702016@st.fasttrack.edu.vn>
2018-06-03 10:37:30 +0700

    QLSV

--- HungNQ/HungHelloWorld/src/fasttrackse/qlsv/entity/SinhVien.java
+++ HungNQ/HungHelloWorld/src/fasttrackse/qlsv/entity/SinhVien.java
@@ -3,27 +3,19 @@ package fasttrackse.qlsv.entity;
 public class SinhVien {
 	private String tenSinhVien;
 	private int namSinh;
+	private String gioiTinh;
 	private int diemLP1;
 	private int diemLP2;
 	private int diemLP3;
+	private double diemTrungBinh;
 
-	public SinhVien() {
-		//
-	}
-
-	public SinhVien(String tenSinhVien, int namSinh) {
+	public SinhVien(String tenSinhVien, int namSinh, String gioiTinh, int diemLP1, int diemLP2, int diemLP3) {
 		this.tenSinhVien = tenSinhVien;
 		this.namSinh = namSinh;
-
-	}
-
-	public SinhVien(String tenSinhVien, int namSinh, int diemLP1, int diemLP2, int diemLP3) {
-		this.tenSinhVien = tenSinhVien;
-		this.namSinh = namSinh;
+		this.gioiTinh = gioiTinh;
 		this.diemLP1 = diemLP1;
 		this.diemLP2 = diemLP2;
 		this.diemLP3 = diemLP3;
-
 	}
 
 	public String getTenSinhVien() {
@@ -32,6 +24,14 @@ public class SinhVien {
 
 	public void setTenSinhVien(String tenSinhVien) {
 		this.tenSinhVien = tenSinhVien;
+	}
+
+	public int getGioiTinh() {
+		return namSinh;
+	}
+
+	public void getGioiTinh(String gioiTinh) {
+		this.gioiTinh = gioiTinh;
 	}
 
 	public int getNamSinh() {
@@ -66,11 +66,34 @@ public class SinhVien {
 		this.diemLP3 = diemLP3;
 	}
 
-	public double tinhDiemTrungBinhMon() {
-		return (diemLP1 + diemLP2 + diemLP3) * 1.0 / 3;
+	public double tinhDiemTrungBinh() {
+
+		return diemTrungBinh = Math.round((diemLP1 + diemLP2 + diemLP3) * 1.0 / 3);
+
+	}
+
+	// xếp loại
+	public String xepLoaiSinhVien() {
+
+		String ketQua = " ";
+
+		if (diemTrungBinh > 8.5) {
+			ketQua = "GIỎI";
+		} else if (diemTrungBinh >= 7 && diemTrungBinh <= 8.5) {
+			ketQua = "KHÁ";
+		} else if (diemTrungBinh >= 5 && diemTrungBinh < 7) {
+			ketQua = "TBÌNH";
+		} else if (diemTrungBinh < 5) {
+			ketQua = "YẾU";
+
+		}
+		return ketQua;
+
 	}
 
 	public String toString() {
-		return "Sinh Viên: " + this.tenSinhVien + " - " + this.namSinh;
+
+		return "\t\t" + this.tenSinhVien + "\t\t" + this.namSinh + "\t  " + this.gioiTinh + "\t   " + this.diemLP1
+				+ "\t    " + this.diemLP2 + "\t     " + this.diemLP3 + "\t     ";
 	}
 }
