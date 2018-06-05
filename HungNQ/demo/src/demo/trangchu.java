288959d294391a6cbfa0b5337458219353ad1526
Nguyễn Quốc Hưng <ffse1702016@st.fasttrack.edu.vn>
2018-06-05 22:49:52 +0700

    QuanLyTienDien

--- HungNQ/demo/src/demo/trangchu.java
+++ HungNQ/demo/src/demo/trangchu.java
@@ -2,8 +2,10 @@ package demo;
 
 public class trangchu {
 	public static void main(String[] args) {
+		
 		hinhchunhat hinh1 = new hinhchunhat(2,3);
 		hinh1.hienthiDT();
+		
 		hinhchunhat hinh2 = new hinhchunhat(3,3);
 		hinh1.hienthiDT();
 		
