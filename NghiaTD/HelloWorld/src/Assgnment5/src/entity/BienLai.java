4d41bf0be838e4ef5152fd56c9a3a506ec595f25
truongdainghia <36950369+truongdainghia@users.noreply.github.com>
2018-06-08 02:18:52 +0700

    up

--- NghiaTD/HelloWorld/src/Assgnment5/src/entity/BienLai.java
+++ NghiaTD/HelloWorld/src/Assgnment5/src/entity/BienLai.java
@@ -1,41 +1,54 @@
-package entity;
+package Assgnment5.src.entity;
 
 public class BienLai {
-	private String kyTieuThu;
-	private int chiSoCu;
-	private int chiSoMoi;
+	protected String maCongTo;
+	protected int chuKyThang;
+	protected int chuKyNam;
+	protected int csDauKy;
+	protected int csCuoiKy;
 	
-	public BienLai(String kyTieuThu,int chiSoCu,int chiSoMoi) {
-		this.kyTieuThu=kyTieuThu;
-		this.chiSoCu=chiSoCu;
-		this.chiSoMoi=chiSoMoi;
-	}
-	public String getkyTieuThu() {
-		return kyTieuThu;
-	}
-	public void setkyTieuThu() {
-		this.kyTieuThu=kyTieuThu;
-	}
-	public int getchiSoCu() {
-		return chiSoCu;
-	}
-	public void setchiSoCu() {
-		this.chiSoCu=chiSoCu;
+	public BienLai(String maCongTo,int chuKyThang,int chuKyNam,int csDauKy,int csCuoiKy) {
+		this.maCongTo=maCongTo;
+		this.chuKyThang=chuKyThang;
+		this.chuKyNam=chuKyNam;
+		this.csDauKy=csDauKy;
+		this.csCuoiKy=csCuoiKy;
 	}
 	
-	public int getchiSoMoi() {
-		return chiSoMoi;
+	
+	public String getMaCongTo() {
+		return maCongTo;
 	}
-	public void setchiSoMoi() {
-		this.chiSoMoi= chiSoMoi;
+	public void setMaCongTo(String maCongTo) {
+		this.maCongTo = maCongTo;
 	}
-	public double sotienphaitra() {
-		double sotientra=0;
-		if(chiSoMoi>chiSoCu) {
-			sotientra=(chiSoMoi-chiSoCu)*3000;
-		}
-		return sotientra;
-		
-		
+	public int getChuKyThang() {
+		return chuKyThang;
 	}
+	public void setChuKyThang(int chuKyThang) {
+		this.chuKyThang = chuKyThang;
+	}
+	public int getChuKyNam() {
+		return chuKyNam;
+	}
+	public void setChuKyNam(int chuKyNam) {
+		this.chuKyNam = chuKyNam;
+	}
+	public int getCsDauKy() {
+		return csDauKy;
+	}
+	public void setCsDauKy(int csDauKy) {
+		this.csDauKy = csDauKy;
+	}
+	public int getCsCuoiKy() {
+		return csCuoiKy;
+	}
+	public void setCsCuoiKy(int csCuoiKy) {
+		this.csCuoiKy = csCuoiKy;
+	}
+	public long tinhTongTien() {
+		return (this.csCuoiKy-this.csDauKy)*300;
+	}
+
+
 }
--- NghiaTD/HelloWorld/src/Assgnment5/src/main/Main.java
+++ NghiaTD/HelloWorld/src/Assgnment5/src/main/Main.java
@@ -1,48 +1,123 @@
-package main;
-
+package Assgnment5.src.main;
+import java.util.ArrayList;
 import java.util.Scanner;
-import entity.*;
+import Assgnment5.src.entity.*;
 public class Main {
 
+	static ArrayList<KhachHang> dsKH = new ArrayList<KhachHang>();
+	static Scanner myInput = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int N;
-		Scanner nhap = new Scanner(System.in);
-		System.out.print("Nhập số lượng khách hàng : ");
-		N = nhap.nextInt();
-		KhachHang[] khachhang=new KhachHang[N];
-		BienLai[] bienlai= new BienLai[N];
-		for(int i=0;i<khachhang.length;i++) {
-			System.out.print("Nhập mã khách hàng");
-			nhap.nextLine();
-			String maKhachHang=nhap.nextLine();
-			
-			System.out.print("Nhập tên khách hàng");
-			String tenKhachHang=nhap.nextLine();
-			
-			System.out.print("Nhập địa khách hàng");
-			String diaChi=nhap.nextLine();
-			
-			System.out.print("Nhập mã công tơ");
-			int maCongTo=nhap.nextInt();
-			
-			System.out.print("Nhập kỳ tiêu thụ");
-			String kyTieuThu=nhap.nextLine();
-			nhap.nextLine();
-			
-			System.out.print("Nhập chỉ số cũ");
-			int chiSoCu=nhap.nextInt();
-			
-			System.out.print("Nhập chỉ số mới");
-			int chiSoMoi=nhap.nextInt();
-			khachhang[i]= new KhachHang(maKhachHang,tenKhachHang,diaChi,maCongTo);
-			bienlai[i]=new BienLai(kyTieuThu,chiSoCu,chiSoMoi);
-		}
-		System.out.println("\t \t \t\tDANH SÁCH KHÁCH HÀNG");
-		System.out.println("\t \t \t *********************************");
-		for(int i=0;i<khachhang.length;i++) {
-			System.out.print("Khách hàng thứ:"+(i+1)+":"+khachhang[i].toString()+"Kỳ tiêu thụ:"+bienlai[i].getkyTieuThu()+"chỉ số cũ:"+bienlai[i].getchiSoCu()+"Chỉ số mới:"+bienlai[i].getchiSoMoi()+"số tiền phải trả:"+bienlai[i].sotienphaitra()+" VNĐ");
+		menu();
+}
+	public static void menu() {
+		int myOption = 0;
+		while (myOption != 5) {
+			try {
+				System.out.println("\nHệ thống quản lý Tiền Điện");
+				System.out.println("---------------------------------------");
+				System.out.println("1. Nhập danh sách Khách Hàng");
+				System.out.println("2. Nhập thông tin Biên Lai");
+				System.out.println("3. In báo cáo tiêu thụ điện");
+				System.out.println("4. Xoá 1 khách hàng");
+				System.out.println("5. Kết thúc chương trình");
+				System.out.println("---------------------------------------");
+				System.out.print("Mời bạn chọn chức năng: ");
+
+				myOption = myInput.nextInt();
+				if (myOption == 1) {
+					// Generate data
+					creatDaTaKH();
+				} else if (myOption == 2) {
+					// Sort student list by Name
+					creaDaTaBienLai();
+				} else if (myOption == 3) {
+					// Sort student list by Name
+					printReport("Thống kê tình hình tiêu thụ điện");
+				} else if (myOption == 4) {
+					xoaKH(1);
+					// Sort student list by Name
+					printReport("Thống kê tình hình tiêu thụ điện");
+				} else if (myOption == 5) {
+					// Hiển thị thông báo kết thúc
+					ketThuc();
+				} else {
+					throw new Exception();
+				}
+			} catch (Exception e) {
+				System.out.println("Chỉ nhập từ 1 đến 5, nhập lại nhé bạn hiền!");
+			} finally {
+				if (myOption != 5) {
+					backToMainMenu();
+				}
+			}
 		}
 	}
+	public static void creatDaTaKH(){
+		dsKH = new ArrayList<KhachHang>();
+		dsKH.add(new KhachHang("SV001", "Nguyễn Văn Sỹ", "Sơn Trà,Đà Nẵng"));
+		dsKH.add(new KhachHang("SV002", "Ngô Thế Toàn", "Hải CHâu,Đà Nẵng"));
+		dsKH.add(new KhachHang("SV003", "Cao Ngọc Khánh", "Liên CHiểu,Đà Nẵng"));
+		dsKH.add(new KhachHang("SV004", "Trần Xuân kì", "Thanh Khê,Đà Nẵng"));
+		dsKH.add(new KhachHang("SV005", "Hứa Văn Lều", "Sơn Trà,Đà Nẵng"));
+		
+	}
+	
+	public static void creaDaTaBienLai(){
+		//biên lai kh 1
+		ArrayList<BienLai> blKH1 = new  ArrayList<BienLai>();
+		blKH1.add(new BienLai("CK1",1,2018,0,100));
+		blKH1.add(new BienLai("CK1",2,2018,110,140));
+		blKH1.add(new BienLai("CK1",3,2018,140,190));
+		blKH1.add(new BienLai("CK1",4,2018,190,210));
+		dsKH.get(0).setDsBienLai(blKH1);
+		dsKH.get(0).addtodsBienLai(new BienLai("Ck1", 5, 2018, 200, 250));
+		
+		// Biên lai khách hàng 2
+		ArrayList<BienLai> blKH2 = new  ArrayList<BienLai>();
+		blKH2.add(new BienLai("CK2",3,2018,0,170));
+		blKH2.add(new BienLai("CK2",4,2018,170,270));
+		dsKH.get(1).setDsBienLai(blKH2);
+		
+		// Biên lai khách hàng 3
+				ArrayList<BienLai> blKH3 = new  ArrayList<BienLai>();
+				blKH3.add(new BienLai("CK3",4,2018,0,120));
+				blKH3.add(new BienLai("CK3",5,2018,120,170));
+				dsKH.get(2).setDsBienLai(blKH3);
+				
+				// Biên lai khách hàng 4
+				dsKH.get(3).addtodsBienLai(new BienLai("Ck4", 6, 2018, 138, 160));
+				// Biên lai khách hàng 4
+				dsKH.get(4).addtodsBienLai(new BienLai("Ck5", 7, 2018, 150, 160));
+	
+	}
+	public static void ketThuc() {
+		System.out.println("Kết thúc chương trình, cám ơn bạn đã sử dụng!!!");
+	}
+	public static void xoaKH(int i) {
+		if(!dsKH.isEmpty()) {
+			dsKH.remove(i);
+		}
+	}
+	public static void backToMainMenu() {
+		myInput.nextLine();
+		System.out.println("Ấn Enter để về menu chính");
+		myInput.nextLine();
+	}
+	public static void printReport(String title) {
+		System.out.println("\n"+title);
+		System.out.println(
+				"--------------------------------------------------------------------------------------------");
+		for(KhachHang x :dsKH) {
+			System.out.println(x.getMaKH()+":"+x.getTenKH()+"\t");
+			for(BienLai o: x.getDsBienLai()) {
+				System.out.println("\t\t\t" + o.getMaCongTo() + " - " + o.getChuKyNam() + "/" + o.getChuKyThang() + " - "
+						+ o.getCsDauKy() + "\t:\t" + o.getCsCuoiKy() + "\t->\t" + o.tinhTongTien());
+			}
+		}
+		System.out.println(
+				"--------------------------------------------------------------------------------------------");
+	}
+	}
 
-}
