cbd87c1421c7db6d48a2b7ea86fb29848408f016
vanducsinh <ffse1704010@st.fasttrack.edu.vn>
2018-06-08 15:17:28 +0700

    asm 6

---
+++ Sinhvd/SinhHeloworld/src/assignment1/assignment.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 5;
+int b = 7;
+int c = a+b;
+System.out.println("tổng " + c);
+	}
+
+}
---
+++ Sinhvd/danhsachnv/src/quanly/QunaLy.java
@@ -0,0 +1,27 @@
+package quanly;
+ import java.util.Scanner;
+
+import danhsachnv.danhsachnv;
+ 
+public class QunaLy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int N;
+	Scanner sc = new Scanner(System.in);         {
+    
+			System.out.println("Thong tin hs 1");
+		     N=sc.nextInt();
+		    danhsachnv[] sinhvien= new danhsachnv[N];
+			for(int i=0;i<N;i++ {
+				danhsachnv[i]=new sinhvien()
+				sinhvien[i]=input();
+			}
+
+			
+			
+			
+		}
+	}
+
+}
