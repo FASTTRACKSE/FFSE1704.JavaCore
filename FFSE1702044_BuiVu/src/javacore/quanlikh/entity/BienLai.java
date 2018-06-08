32d4092b2f8ae339b91a56cbe65c1fa661a5303e
bùi vũ <FFSE1702044@st.fasttrack.edu.vn>
2018-06-08 13:58:35 +0700

    readme.md

--- FFSE1702044_BuiVu/src/javacore/quanlikh/entity/BienLai.java
+++ FFSE1702044_BuiVu/src/javacore/quanlikh/entity/BienLai.java
@@ -1,30 +1,43 @@
 package javacore.quanlikh.entity;
 
-public class BienLai extends ThongTinKhachHang{
-	private float chiSoCu;
-	private float chiSoMoi;
-	public BienLai(String tenKhachHang, int maKhachHang,String diaChiKhachHang,int maCongTo,float chiSoCu,float chiSoMoi) {
-		super(tenKhachHang,maKhachHang,diaChiKhachHang,maCongTo);
+public class BienLai{
+	protected int chiSoCu;
+	protected int chiSoMoi;
+	protected int ckThang;
+	protected int ckNam;
+	
+	public BienLai(int chiSoCu,int chiSoMoi,int ckThang, int ckNam) {
 		this.chiSoCu=chiSoCu;
 		this.chiSoMoi=chiSoMoi;
+		this.ckThang=ckThang;
+		this.ckNam=ckNam;
 	}
-	public float getChiSoCu() {
+	public int getCkThang() {
+		return ckThang;
+	}
+	public void setCkThang(int ckThang) {
+		this.ckThang = ckThang;
+	}
+	public int getCkNam() {
+		return ckNam;
+	}
+	public void setCkNam(int ckNam) {
+		this.ckNam = ckNam;
+	}
+	public int getChiSoCu() {
 		return chiSoCu;
 	}
-	public void setChiSoCu(float chiSoCu) {
+	public void setChiSoCu(int chiSoCu) {
 		this.chiSoCu=chiSoCu;
 	}
-	public float getChiSoMoi() {
+	public int getChiSoMoi() {
 		return chiSoMoi;
 	}
-	public void setChiSoMoi(float chiSoMoi) {
+	public void setChiSoMoi(int chiSoMoi) {
 		this.chiSoMoi=chiSoMoi;
 	}
-	public float tinhTienDien() {
+	public long tinhTienDien() {
 		return (chiSoMoi-chiSoCu)*3000;
-	}
-	public String inBienLai() {
-		return "\n"+getMaKhachHang()+ "\t"+getThongTinKhachHang()+ "\t "+getDiaChiKhachHang()+"\t"+getChiSoCu()+"\t "+getChiSoMoi()+"\t  "+tinhTienDien()+"VNĐ";
 	}
 }
 
