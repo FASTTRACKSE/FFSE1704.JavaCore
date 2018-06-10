db6e5d85e40fab5213d889a966a6096b238f5c42
phanthanhtuan <ffse1702040@st.fasttrack.edu.vn>
2018-06-11 01:35:47 +0700

    asm6- QuanLyTienDien

--- TuanPT/BaiTapMang/src/fasttrackse/javacore/assignment3/TaoMenu.java
+++ TuanPT/BaiTapMang/src/fasttrackse/javacore/assignment3/TaoMenu.java
@@ -62,7 +62,7 @@ public class TaoMenu {
 		}
 	}
 
-	// Nhập mảng 
+	// Nhập mảng
 	public static void BaiTapMang() {
 		// Phần khai báo
 		int N; // tổng số phần tử mảng
@@ -107,55 +107,51 @@ public class TaoMenu {
 
 		}
 	}
-	
-	//Sắp xếp mảng
+
+	// Sắp xếp mảng
 	public static void SapXepMang() {
 		Scanner myInput = new Scanner(System.in);
-	    
-        System.out.print("Nhập số phần tử của mảng: ");
-        int n = myInput.nextInt();
-        // khởi tạo arr
-        int [] arr = new int [n];
-        System.out.print("Nhập các phần tử của mảng: \n");
-        for (int i = 0; i < n; i++) {
-            System.out.printf("a[%d] = ", i);
-            arr[i] = myInput.nextInt();
-        }
-        // sắp xếp dãy số theo thứ tự tăng dần
-        sortASC(arr);
-        System.out.println("Dãy số được sắp xếp tăng dần: ");
-        show(arr);
-    }
-     
-    /**
-     * sắp xếp mảng số nguyên theo thứ tự tăng dần
-     * 
-     * @param arr: mảng các số nguyên
-     * @param n: số phần tử của mảng
-     */
-    public static void sortASC(int [] arr) {
-        int temp = arr[0];
-        for (int i = 0 ; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
-            }
-        }
-    }
-     
-    /**
-     * in các phần tử của mảng ra màn hình
-     * 
-     * @param arr: mảng các số nguyên
-     * @param n: số phần tử của mảng
-     */
-    public static void show(int [] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
+
+		System.out.print("Nhập số phần tử của mảng: ");
+		int n = myInput.nextInt();
+		// khởi tạo arr
+		int[] arr = new int[n];
+		System.out.print("Nhập các phần tử của mảng: \n");
+		for (int i = 0; i < n; i++) {
+			System.out.printf("a[%d] = ", i);
+			arr[i] = myInput.nextInt();
+		}
+		// sắp xếp dãy số theo thứ tự tăng dần
+		sortASC(arr);
+		System.out.println("Dãy số được sắp xếp tăng dần: ");
+		show(arr);
+	}
+
+	/**
+	 * sắp xếp mảng số nguyên theo thứ tự tăng dần
+	 * 
+	 * @param arr:
+	 *            mảng các số nguyên
+	 * @param n:
+	 *            số phần tử của mảng
+	 */
+	public static void sortASC(int[] arr) {
+		int temp = arr[0];
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void show(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 
 	}
 
@@ -176,10 +172,10 @@ public class TaoMenu {
 			switch (chon) {
 			case 1:
 				GiaiPhuongTrinhBacNhat();
-				break; 
+				break;
 			case 2:
 				GiaiPhuongTrinhBacHaiMotAn();
-				break; 
+				break;
 			case 3:
 				BaiTapMang();
 				break;
@@ -192,6 +188,7 @@ public class TaoMenu {
 			}
 			}
 		} while (chon != 0);
+		
 
 	}
 
