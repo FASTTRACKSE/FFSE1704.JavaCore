58d10309d0569d18accfa177345bcb98b25ab1fc
dungkhoa2311 <32055660+dungkhoa2311@users.noreply.github.com>
2018-06-13 15:16:07 +0700

    Update

--- KhoaNN/src/ffse/quanlisinhvien/util/SVComparator.java
+++ KhoaNN/src/ffse/quanlisinhvien/util/SVComparator.java
@@ -8,9 +8,9 @@ public class SVComparator {
 	/* Comparator for sorting the list KhachHang by MaKH */
 	public static Comparator<SinhVien> SVCodeASCComparator = new Comparator<SinhVien>() {
 
-		public int compare(SinhVien kh1, SinhVien kh2) {
-			String codeKH1 = kh1.getHoTen().toUpperCase();
-			String codeKH2 = kh2.getHoTen().toUpperCase();
+		public int compare(SinhVien sv1, SinhVien sv2) {
+			String codeKH1 = sv1.getHoTen().toUpperCase();
+			String codeKH2 = sv2.getHoTen().toUpperCase();
 
 			// ascending order
 			return codeKH1.compareTo(codeKH2);
@@ -54,23 +54,23 @@ public class SVComparator {
 	};
 
 	// /*Comparator for sorting the list by roll no*/
-	// public static Comparator<SinhVien> SinhVienDTBDESComparator = new
-	// Comparator<SinhVien>() {
+	 public static Comparator<SinhVien> SinhVienDTBDESComparator = new
+	 Comparator<SinhVien>() {
 	//
-	// public int compare(SinhVien s1, SinhVien s2) {
+	 public int compare(SinhVien s1, SinhVien s2) {
 	//
-	// float fDTB1 = s1.getDiemTB();
-	// float fDTB2 = s2.getDiemTB();
+	 double fDTB1 = s1.tinhDiemTb();
+	 double fDTB2 = s2.tinhDiemTb();
 	//
 	// /*For ascending order*/
-	// if ((fDTB2 - fDTB1) > 0)
-	// return 1;
-	// else if ((fDTB2 - fDTB1) < 0)
-	// return -1;
-	// else
-	// return 0;
+	 if ((fDTB2 - fDTB1) > 0)
+	 return 1;
+	else if ((fDTB2 - fDTB1) < 0)
+	 return -1;
+	 else
+	 return 0;
 	//
 	// /*For descending order*/
-	// //fDTB1-fDTB2;
-	// }};
+	//fDTB1-fDTB2;
+	}};
 }
