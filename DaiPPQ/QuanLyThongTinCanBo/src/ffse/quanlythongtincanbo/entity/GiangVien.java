9119001603e6b62e01182fec15a6faec0291efbf
QuangDaiPhanPham <ffse1704002@st.fasttrack.edu.vn>
2018-06-19 17:06:38 +0700

    Quản lý cán bộ

--- DaiPPQ/QuanLyThongTinCanBo/src/ffse/quanlythongtincanbo/entity/GiangVien.java
+++ DaiPPQ/QuanLyThongTinCanBo/src/ffse/quanlythongtincanbo/entity/GiangVien.java
@@ -5,32 +5,16 @@
  */
 package ffse.quanlythongtincanbo.entity;
 
-
 public class GiangVien extends NhanVien {
-	private String tenGV;
 	private String khoa;
 	private String trinhDo;
 	private int soTietDayTrongThang;
-	private double heSoLuong;
 	
-	public GiangVien(String tenGV, String khoa, String trinhDo, int soTietDayTrongThang, float heSoLuong) {
-		this.tenGV = tenGV;
+	public GiangVien(String ten, String khoa, String trinhDo, int phuCap, int soTietDayTrongThang, double heSoLuong) {
+		super(ten, phuCap, heSoLuong);
 		this.khoa = khoa;
 		this.trinhDo = trinhDo;
 		this.soTietDayTrongThang = soTietDayTrongThang;
-		this.heSoLuong = heSoLuong;
-	}
-
-	public GiangVien(String tenGV2, String khoa2, String trinhDo2, int soTietDay, double heSoLuong2) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public String getTenGV() {
-		return tenGV;
-	}
-
-	public void setTenGV(String tenGV) {
-		this.tenGV = tenGV;
 	}
 
 	public String getKhoa() {
@@ -48,7 +32,7 @@ public class GiangVien extends NhanVien {
 	public void setTrinhDo(String trinhDo) {
 		this.trinhDo = trinhDo;
 	}
-
+	
 	public int getsoTietDayTrongThang() {
 		return soTietDayTrongThang;
 	}
@@ -57,33 +41,11 @@ public class GiangVien extends NhanVien {
 		this.soTietDayTrongThang = soTietDayTrongThang;
 	}
 
-	public double getHeSoLuong() {
-		return heSoLuong;
-	}
-
-	public void setHeSoLuong(double heSoLuong) {
-		this.heSoLuong = heSoLuong;
-	}
 
 	@Override
 	public double tinhLuong() {
-
-		double luong = 0;
-
-		if ("Cử nhân".equals(getTrinhDo())) {
-			luong = (this.heSoLuong * 730) + 300 + (this.soTietDayTrongThang * 45);
-		} else {
-			if ("Thạc sĩ".equals(getTrinhDo())) {
-				luong = (this.heSoLuong * 730) + 500 + (this.soTietDayTrongThang * 45);
-			} else {
-				if ("Tiến sĩ".equals(getTrinhDo())) {
-					luong = (this.heSoLuong * 730) + 1000 + (this.soTietDayTrongThang * 45);
-				}
-			}
-		}
-
+		double luong = getHeSoLuong()*750 + getPhuCap() + getsoTietDayTrongThang()*45;
 		return luong;
-
 	}
 
 }
