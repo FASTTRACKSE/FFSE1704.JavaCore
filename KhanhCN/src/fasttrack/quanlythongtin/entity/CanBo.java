187c206dbe5bc1893e758c1818b764e761fdb172
khanhFFSE1701011 <32625914+khanhFFSE1701011@users.noreply.github.com>
2018-06-21 11:50:42 +0700

    Up Assignment 8

---
+++ KhanhCN/src/fasttrack/quanlythongtin/entity/CanBo.java
@@ -0,0 +1,47 @@
+package fasttrack.quanlythongtin.entity;
+
+public abstract class CanBo {
+
+	private String hoTen;
+	private int phuCap;
+	private double heSo;
+
+	public abstract double tinhLuong();
+	
+	public CanBo() {
+		super();
+	}
+
+	public CanBo(String hoTen, int phuCap, double heSo) {
+		super();
+		this.hoTen = hoTen;
+		this.phuCap = phuCap;
+		this.heSo = heSo;
+	}
+
+	public String getHoTen() {
+		return hoTen;
+	}
+
+	public void setHoTen(String hoTen) {
+		this.hoTen = hoTen;
+	}
+
+	public double getPhuCap() {
+		return phuCap;
+	}
+
+	public void setPhuCap(int phuCap) {
+		this.phuCap = phuCap;
+	}
+
+	public double getHeSo() {
+		return heSo;
+	}
+
+	public void setHeSo(double heSo) {
+		this.heSo = heSo;
+	}
+	
+	
+}
