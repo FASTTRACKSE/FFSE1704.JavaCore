b29dd3f1f793c0f076bc1ddeb8b831610127ecb1
Nguyễn Quốc Hưng <ffse1702016@st.fasttrack.edu.vn>
2018-06-24 10:19:30 +0700

    up

--- HungNQ/HungHelloWorld/src/fasttrackse/qltt/main/QuanLyThongTin.java
+++ HungNQ/HungHelloWorld/src/fasttrackse/qltt/main/QuanLyThongTin.java
@@ -7,57 +7,48 @@ import java.util.Scanner;
 import fasttrackse.qltt.entity.*;
 
 public class QuanLyThongTin {
-	
-	static ArrayList<CanBo> dsCanBo = new ArrayList<CanBo>();
-	static Scanner myInput = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		while (true) {
-			System.out.println("***********************");
-			System.out.println("*    1.Nhập GV,NV     *");
-			System.out.println("*    2.In ds GV,NV    *");
-			System.out.println("*      3.Kết Thúc     *");
-			System.out.println("***********************");
+		ArrayList<CanBo> arrCanBo = new ArrayList<>();
+		int choose, soCanBo;
+		long tongLuong = 0, luongThapNhat, luong;
+		CanBo canBo = null;
+		Scanner scanner = new Scanner(System.in);
 
-			System.out.println("Mời bạn chọn chương trình: ");
-			int soNguoiDungChon = myInput.nextInt();
-			if (soNguoiDungChon == 1) {
-				Nhap();
-			} else if (soNguoiDungChon == 2) {
-				System.out.println("Nhân Viên");
-			} else if (soNguoiDungChon == 3) {
-				KetThuc();
-			}
-		}
-	}
-	public static void Nhap() {
-		//GiangVien x = new GiangVien(null, null, null, null, null)
-		int N;
-		System.out.print("Nhập Số Lượng Cán Bộ Muốn Nhập");
-		N = myInput.nextInt();
-		for(int i=0;i<N;i++) {
-			System.out.print("Nhập Loại Cán Bộ");
-			int K = myInput.nextInt();
-			try{
-				if(K==1) {
-
-					System.out.print("Mời Bạn Nhập Giảng Viên");
-
-					System.out.print("Nhập Tên GV");
-					
-					String tenCB=myInput.nextLine();
-					
-					
+		System.out.print("Nhập số lượng cán bộ trong trường: ");
+		soCanBo = scanner.nextInt();
+		for (int i = 0; i < soCanBo; i++) {
+			System.out.println("Nhập thông tin cán bộ thứ " + (i + 1) + ":");
+			do {
+				System.out.print("Chọn loại cán bộ (1 - giảng viên, 2 - nhân viên): ");
+				choose = scanner.nextInt();
+				switch (choose) {
+				case 1:
+					canBo = new GiangVien();
+					canBo.nhap();
+					arrCanBo.add(canBo);
+					break;
+				case 2:
+					canBo = new NhanVien();
+					canBo.nhap();
+					arrCanBo.add(canBo);
+					break;
+				default:
+					System.out.println("Chọn không hợp lệ.");
+					break;
 				}
-			}
-			catch(Exception e){
-				System.out.println("Chỉ nhập từ 1 hoặc 2, nhập lại nhé bạn hiền!");
-			}
+			} while (choose < 1 || choose > 3);
 		}
-	}
-	public static void KetThuc() {
-		myInput.close();
-		System.out.println("Cảm ơn bạn đã sử dụng chương trình!!!");
-		System.exit(0);
+
+		System.out.println("Hiển thị danh sách cán cán bộ trong trường: ");
+		for (CanBo cb : arrCanBo) {
+			System.out.println(cb.toString());
+		}
+
+		for (CanBo cb : arrCanBo) {
+			luong = cb.tinhLuong();
+			tongLuong += luong;
+		}
+		System.out.println("Tổng lương phải trả cho cán bộ trong trường = " + tongLuong);
 	}
 }
