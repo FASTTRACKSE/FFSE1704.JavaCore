6e741d453925d842cc1d31fb5860fd9f2569943e
ThanhCL <thanhcl@gmail.com>
2018-06-26 15:00:34 +0700

    update samples

--- ThanhCL/QuanLySinhVien/src/fasttrackse/qlsv/main/QLSV2.java
+++ ThanhCL/QuanLySinhVien/src/fasttrackse/qlsv/main/QLSV2.java
@@ -8,15 +8,15 @@ import java.util.Scanner;
 
 import fasttrackse.qlsv.entity.*;
 
-public class QLSV {
-	static ArrayList<SinhVien> dsSinhVien = new ArrayList<SinhVien>();;
-	static Scanner myInput = new Scanner(System.in);
+public class QLSV2 {
+	ArrayList<SinhVien> dsSinhVien = new ArrayList<SinhVien>();
 
 	public static void main(String[] args) {
-		menu();
-	}
+		QLSV2 ctQLSV1 = new QLSV2();
+		QLSV2 ctQLSV2 = new QLSV2();
 
-	public static void menu() {
+		Scanner myInput = new Scanner(System.in);
+
 		int myOption = 0;
 		while (myOption != 4) {
 			try {
@@ -32,18 +32,19 @@ public class QLSV {
 				myOption = myInput.nextInt();
 				if (myOption == 1) {
 					// Generate data
-					generateData();
+					ctQLSV1.generateData();
 				} else if (myOption == 2) {
 					// Sort student list by Name
-					sortByName();
-					printReport("Danh Sách Sinh Viên FASTTRACKSE - Sắp xếp theo tên");
+					ctQLSV1.sortByName();
+					ctQLSV1.printReport("Danh Sách Sinh Viên FASTTRACKSE - Sắp xếp theo tên");
 				} else if (myOption == 3) {
 					// Sort student list by Name
-					sortByMarks();
-					printReport("Danh Sách Sinh Viên FASTTRACKSE - Sắp xếp theo điểm TB");
+					ctQLSV1.sortByMarks();
+					ctQLSV1.printReport("Danh Sách Sinh Viên FASTTRACKSE - Sắp xếp theo điểm TB");
 				} else if (myOption == 4) {
 					// Hiển thị thông báo kết thúc
 					ketThuc();
+					myInput.close();
 				} else {
 					throw new Exception();
 				}
@@ -51,7 +52,7 @@ public class QLSV {
 				System.out.println("Chỉ nhập từ 1 đến 4, nhập lại nhé bạn hiền!");
 			} finally {
 				if (myOption != 4) {
-					backToMainMenu();
+					ctQLSV2.backToMainMenu();
 				}
 			}
 		}
@@ -61,13 +62,11 @@ public class QLSV {
 		System.out.println("Kết thúc chương trình, cám ơn bạn đã sử dụng!!!");
 	}
 
-	public static void backToMainMenu() {
-		myInput.nextLine();
+	public void backToMainMenu() {
 		System.out.println("Ấn Enter để về menu chính");
-		myInput.nextLine();
 	}
 
-	public static void generateData() {
+	public void generateData() {
 		// Danh sách Môn Học
 		MonHoc LP1 = new MonHoc("LP1", "Nhập Môn Lập Trình");
 		MonHoc LP2 = new MonHoc("LP2", "Lập Trình Web FrontEnd");
@@ -75,7 +74,7 @@ public class QLSV {
 		MonHoc LP4 = new MonHoc("LP4", "Lập Trình Java EE");
 		MonHoc LP5 = new MonHoc("LP5", "Lập Trình Android");
 
-		// Nhập 
+		// Nhập
 		ArrayList<DiemMonHoc> diemSV1 = new ArrayList<DiemMonHoc>();
 		diemSV1.add(new DiemMonHoc(LP1, 10));
 		diemSV1.add(new DiemMonHoc(LP2, 10));
@@ -94,8 +93,7 @@ public class QLSV {
 		diemSV3.add(new DiemMonHoc(LP3, 9));
 
 		dsSinhVien = new ArrayList<SinhVien>();
-		SinhVien x = new SinhVien();
-		
+
 		dsSinhVien.add(sv1);
 		dsSinhVien.add(sv2);
 		dsSinhVien.add(new SinhVien("Chu Cảnh", "Chiêu", 1992, diemSV3));
@@ -108,7 +106,7 @@ public class QLSV {
 		}));
 	}
 
-	public static void sortByName() {
+	public void sortByName() {
 		if (dsSinhVien.size() > 0) {
 			Collections.sort(dsSinhVien, new Comparator<SinhVien>() {
 				@Override
@@ -119,7 +117,7 @@ public class QLSV {
 		}
 	}
 
-	public static void sortByMarks() {
+	public void sortByMarks() {
 		if (dsSinhVien.size() > 0) {
 
 			Collections.sort(dsSinhVien, new Comparator<SinhVien>() {
@@ -139,7 +137,7 @@ public class QLSV {
 		}
 	}
 
-	public static void printReportHeader(String title) {
+	public void printReportHeader(String title) {
 		System.out.println("\n" + title);
 		System.out.println(
 				"STT Họ Tên              Năm Sinh       LP1       LP2       LP3       LP4       LP5       DTB");
@@ -147,58 +145,58 @@ public class QLSV {
 				"--------------------------------------------------------------------------------------------");
 	}
 
-	public static void printReportFooter() {
+	public void printReportFooter() {
 		System.out.println(
 				"--------------------------------------------------------------------------------------------");
 	}
 
-	public static void printReport(String title) {
+	public void printReport(String title) {
 		printReportHeader(title);
 
 		int stt = 0;
-		
+
 		if (dsSinhVien.size() == 0) {
 			System.out.println("Không có sinh viên nào");
-		}
-		else {
+		} else {
 
-		for (SinhVien sv : dsSinhVien) {
-			System.out.printf("%03d %-20s%8d", ++stt, sv.getHoSinhVien() + " " + sv.getTenSinhVien(), sv.getNamSinh());
+			for (SinhVien sv : dsSinhVien) {
+				System.out.printf("%03d %-20s%8d", ++stt, sv.getHoSinhVien() + " " + sv.getTenSinhVien(),
+						sv.getNamSinh());
 
-			String sLP1 = "x";
-			String sLP2 = "x";
-			String sLP3 = "x";
-			String sLP4 = "x";
-			String sLP5 = "x";
-			String sDTB = "x";
-			DecimalFormat formatter = new DecimalFormat("#,###.0");
-			for (DiemMonHoc x : sv.getDiemThi()) {
-				String temp = formatter.format(x.getDiemThi());
-				switch (x.getMonHoc().getMaMonHoc()) {
-				case "LP1":
-					sLP1 = temp;
-					break;
+				String sLP1 = "x";
+				String sLP2 = "x";
+				String sLP3 = "x";
+				String sLP4 = "x";
+				String sLP5 = "x";
+				String sDTB = "x";
+				DecimalFormat formatter = new DecimalFormat("#,###.0");
+				for (DiemMonHoc x : sv.getDiemThi()) {
+					String temp = formatter.format(x.getDiemThi());
+					switch (x.getMonHoc().getMaMonHoc()) {
+					case "LP1":
+						sLP1 = temp;
+						break;
 
-				case "LP2":
-					sLP2 = temp;
-					break;
+					case "LP2":
+						sLP2 = temp;
+						break;
 
-				case "LP3":
-					sLP3 = temp;
-					break;
+					case "LP3":
+						sLP3 = temp;
+						break;
 
-				case "LP4":
-					sLP4 = temp;
-					break;
+					case "LP4":
+						sLP4 = temp;
+						break;
 
-				case "LP5":
-					sLP5 = temp;
-					break;
+					case "LP5":
+						sLP5 = temp;
+						break;
+					}
 				}
+				sDTB = formatter.format(sv.tinhDiemTrungBinhMon());
+				System.out.printf("%10s%10s%10s%10s%10s%10s%n", sLP1, sLP2, sLP3, sLP4, sLP5, sDTB);
 			}
-			sDTB = formatter.format(sv.tinhDiemTrungBinhMon());
-			System.out.printf("%10s%10s%10s%10s%10s%10s%n", sLP1, sLP2, sLP3, sLP4, sLP5, sDTB);
-		}
 		}
 
 		printReportFooter();
