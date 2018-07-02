1e36d9d0b86f0f543e99ed6de1dfe13cc686d9d7
bùi vũ <FFSE1702044@st.fasttrack.edu.vn>
2018-07-02 16:42:55 +0700

    readme.md

---
+++ FFSE1702044_BuiVu/src/ffse/qlkhsql/utity/myException.java
@@ -0,0 +1,40 @@
+package ffse.qlkhsql.utity;
+
+public class myException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int err;
+
+	public myException(int a) {
+		err = a;
+	}
+
+	public String toString() {
+		String msg = "Lỗi không xác định, mời chạy lại !!!";
+		switch (err) {
+		case 1:
+			msg = "Vui Lòng Nhập ID Khách Hàng!!! ";
+			break;
+		case 2:
+			msg = "Bạn đã nhập sai, hãy chọn lại 1 => 7 tương ứng !!!";
+			break;
+		case 3:
+			msg = "Vui lòng nhập lại số nguyên dương !!!";
+			break;
+		case 4:
+			msg = "Mã cán bộ đã bị trùng, vui lòng nhập lại !!!";
+			break;
+		case 5:
+			msg = "Vui Lòng Nhập Từ 1-15 kí Tự";
+			break;
+		case 6:
+			msg = "Vui lòng Nhập Số Lượng!!! ";
+			break;
+		}
+		return msg;
+	}
+
+}
+
