55ee5b34987a170f72412277aaadb45a44679f54
davidvy <36950456+davidvy@users.noreply.github.com>
2018-07-04 15:11:03 +0700

    bài tập tạo giao diện

---
+++ vyng/chuongtrinhgiaodien/src/main/GiaoDien.java
@@ -0,0 +1,13 @@
+package main;
+
+import ui.LoginUI;
+
+public class GiaoDien {
+
+	public static void main(String[] args) {
+		LoginUI myLoginUI = new LoginUI("Màn hình Login");
+		myLoginUI.showWindow();
+
+	}
+
+}
