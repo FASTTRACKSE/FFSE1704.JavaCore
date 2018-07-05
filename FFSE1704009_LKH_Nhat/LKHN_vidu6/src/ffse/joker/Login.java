07822441a88b123a3c9a613f77daa0225fd125d3
LeeKharHoongNhat <36950051+LeeKharHoongNhat@users.noreply.github.com>
2018-07-05 14:04:57 +0700

    Update Login.java

--- FFSE1704009_LKH_Nhat/LKHN_vidu6/src/ffse/joker/Login.java
+++ FFSE1704009_LKH_Nhat/LKHN_vidu6/src/ffse/joker/Login.java
@@ -5,7 +5,7 @@ import ffse.ui.MyFlowLayoutUI;
 public class Login {
 
 	public static void main(String[] args) {
-		MyFlowLayoutUI myUI = new MyFlowLayoutUI("Login");
+		MyFlowLayoutUI myUI = new MyFlowLayoutUI("Chương Trình Giải Phương Trình Bậc Nhất");
 		myUI.showWindow();
 		
 	}
