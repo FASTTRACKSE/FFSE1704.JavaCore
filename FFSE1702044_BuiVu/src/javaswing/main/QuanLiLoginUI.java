722f57f19980b11093c5b448a2fbafaec4feb81c
bùi vũ <FFSE1702044@st.fasttrack.edu.vn>
2018-07-05 14:10:51 +0700

    readme.md

---
+++ FFSE1702044_BuiVu/src/javaswing/main/QuanLiLoginUI.java
@@ -0,0 +1,14 @@
+package javaswing.main;
+
+import javaswing.ui.*;
+
+public class QuanLiLoginUI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		LoginUI myLoginUI = new LoginUI("Màn hình Login");
+		myLoginUI.showWindow();
+	}
+
+}
