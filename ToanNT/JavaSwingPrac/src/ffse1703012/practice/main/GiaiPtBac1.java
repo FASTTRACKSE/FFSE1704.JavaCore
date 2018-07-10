03e31c48cb1500d95b4456d684e43ffe92c462e4
FFSE1703012 <37531493+FFSE1703012@users.noreply.github.com>
2018-07-10 17:11:54 +0700

    Assignment 12

---
+++ ToanNT/JavaSwingPrac/src/ffse1703012/practice/main/GiaiPtBac1.java
@@ -0,0 +1,13 @@
+package ffse1703012.practice.main;
+
+import ffse1703012.practice.ui.JavaDesktop;
+
+public class GiaiPtBac1 {
+
+	public static void main(String[] args) {
+		JavaDesktop myLoginUI = new JavaDesktop("Java Desktop - Chương trình giải Phương trình bậc 1");
+		myLoginUI.showWindows();
+
+	}
+
+}
---
+++ ToanNT/QuanLySinhVienJavaSwing/src/ffse1703012/main/QuanLySinhVien.java
@@ -0,0 +1,9 @@
+package ffse1703012.main;
+import ffse1703012.ui.*;
+public class QuanLySinhVien {
+	public static void main(String[] args) {
+		QuanLySinhVienUi qLSV = new QuanLySinhVienUi("Chương trình quản lý sinh viên");
+		qLSV.showWindows();
+	}
+	
+}
